780f6660214655b2d1f7ebae71fdbcc2aff242a4
naoki kodama <naoki.koda@gmail.com>
2013-06-01 23:51:03 +0900

    fix #65. unsupport mobile site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class ApplicationController < ActionController::Base
-  include Jpmobile::ViewSelector
+#  include Jpmobile::ViewSelector
   protect_from_forgery
 
   before_filter :authenticate_user!
